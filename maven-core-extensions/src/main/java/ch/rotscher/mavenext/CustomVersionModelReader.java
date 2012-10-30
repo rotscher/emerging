ee8597b97b86e3040bde7acf841c6d27ce6256d7
rotscher <roger@rotscher.ch>
2012-10-30 21:08:20 +0100

    code and logger optimizations

--- maven-core-extensions/src/main/java/ch/rotscher/mavenext/CustomVersionModelReader.java
+++ maven-core-extensions/src/main/java/ch/rotscher/mavenext/CustomVersionModelReader.java
@@ -33,23 +33,22 @@ public class CustomVersionModelReader extends DefaultModelReader implements Mode
 
     @Override
     public Model read(InputStream input, Map<String, ?> options) throws IOException {
+        Model model = super.read(input, options);
         // this property is set from the command line with -Drelease.version
         String version = System.getProperty("release.version");
         if (version == null || version.trim().length() < 1) {
-            return super.read(input, options);
+            return model;
         }
 
-        Model model = super.read(input, options);
-        logger.info(String.format("current module: %s%n", model));
         String currentGroupId = getGroupId(model);
-        GroupIdOfRootPom groupId = GroupIdOfRootPom.getInstance(currentGroupId);
+        GroupIdOfRootPom groupId = GroupIdOfRootPom.getInstance(currentGroupId, logger);
 
         if (currentGroupId.startsWith(groupId.groupId)) {
-            logger.info(String.format("  changing version of: %s:%s:%s%n", model.getGroupId(), model.getArtifactId(), model.getVersion()));
+            logger.info(String.format("changing version of  %s  to %s", model, version));
             model.setVersion(version);
             Parent parent = model.getParent();
             if (parent != null && parent.getGroupId().startsWith(groupId.groupId)) {
-                logger.debug(String.format("  %s:%s:%s (%s)%n", parent.getGroupId(), parent.getArtifactId(), parent.getVersion(), parent));
+                logger.info(String.format("changing version of parent  %s  to %s", parent, version));
                 parent.setVersion(version);
             }
         }
@@ -60,7 +59,7 @@ public class CustomVersionModelReader extends DefaultModelReader implements Mode
             for (Dependency dep : model.getDependencies()) {
                 String depVersion = dep.getVersion();
                 if (depVersion != null && depVersion.contains("SNAPSHOT")) {
-                    String errorMsg = String.format("there is a snapshot dependency (%s) in %s%n", dep, model.getPomFile().getPath());
+                    String errorMsg = String.format("there is a snapshot dependency (%s) in %s", dep, model.getPomFile().getPath());
                     logger.error(errorMsg);
                     throw new IllegalArgumentException(errorMsg);
                 }
@@ -86,6 +85,7 @@ public class CustomVersionModelReader extends DefaultModelReader implements Mode
         }
 
         // TODO: return null if no groupId can be found!
+        logger.warn(String.format("no groupId found for model %s (no clue why this happens :-(. But has no negative affect :-) Maybe this is the super pom!!??", model));
         return "n/A";
     }
 
@@ -113,9 +113,10 @@ public class CustomVersionModelReader extends DefaultModelReader implements Mode
             this.groupId = groupId;
         }
 
-        public static GroupIdOfRootPom getInstance(String groupId) {
+        public static GroupIdOfRootPom getInstance(String groupId, Logger logger) {
             if (INSTANCE == null) {
                 INSTANCE = new GroupIdOfRootPom(groupId);
+                logger.info(String.format("initialize groupId to '%s', the version of all modules starting with this groupdId will be changed!", groupId));
             }
 
             return INSTANCE;
