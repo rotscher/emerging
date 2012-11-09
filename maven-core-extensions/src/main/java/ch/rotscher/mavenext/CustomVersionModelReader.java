7a158db821b05287865ff2c6149a578397c48525
rotscher <roger@rotscher.ch>
2012-11-10 00:02:39 +0100

    tweak improvments of snapshot check feature

--- maven-core-extensions/src/main/java/ch/rotscher/mavenext/CustomVersionModelReader.java
+++ maven-core-extensions/src/main/java/ch/rotscher/mavenext/CustomVersionModelReader.java
@@ -1,24 +1,5 @@
 package ch.rotscher.mavenext;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -49,8 +30,12 @@ import org.codehaus.plexus.logging.Logger;
  * This class helps releasing a project (from a maven point of view, that means not having "SNAPSHOT" in the version) without actually changing (and committing)
  * the pom.xml
  * </p>
+ * <p>
+ * Copyright by Roger Brechbühl, 2012
+ * </p>
  * 
  * @author Roger Brechbühl
+ * 
  */
 @Component(role = ModelReader.class, hint = "custom-version-model-reader")
 public class CustomVersionModelReader extends DefaultModelReader implements ModelReader {
@@ -65,7 +50,7 @@ public class CustomVersionModelReader extends DefaultModelReader implements Mode
     @Override
     public Model read(InputStream input, Map<String, ?> options) throws IOException {
         Model model = super.read(input, options);
-        // this property is set from the command line with -Drelease.version
+        // this property is set from the command line with -Dversion.override
         String version = System.getProperty(MAVENEXT_RELEASE_VERSION);
         if (version == null || version.trim().length() < 1) {
             return model;
@@ -99,10 +84,12 @@ public class CustomVersionModelReader extends DefaultModelReader implements Mode
                         pomFilePath = pomFile.getPath();
                     }
                     String errorMsg = String.format("there is a snapshot dependency (%s) in %s (%s)", dep, model, pomFilePath);
-                    logger.error(errorMsg);
                     if (failOnError == Boolean.TRUE) {
+                        logger.error(errorMsg);
                         // wrap in a IOException as this one is thrown in the signature
                         throw new IOException(new MavenExecutionException(errorMsg, model.getPomFile()));
+                    } else {
+                        logger.warn(errorMsg);
                     }
                 }
             }
