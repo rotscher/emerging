d3fa29a9fb5f415757388f5d4a6d2e93ffc83679
rotscher <info@rotscher.ch>
2013-09-25 00:51:02 +0200

    implemented version replacement based on xml instead of simple text replacement

--- plugins/install-custom-version-plugin/src/main/java/ch/rotscher/maven/plugins/InstallWithVersionOverrideMojo.java
+++ plugins/install-custom-version-plugin/src/main/java/ch/rotscher/maven/plugins/InstallWithVersionOverrideMojo.java
@@ -19,25 +19,28 @@ package ch.rotscher.maven.plugins;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.install.InstallMojo;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.util.FileUtils;
-import org.codehaus.plexus.util.IOUtil;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Field;
 
 /**
  * extending the original install mojo for the version.override feature
- * 
+ *
+ * @author Roger Brechbuehl
  * @extendsPlugin install
  * @extendsGoal install
  * @goal install
- * @author Roger Brechbuehl
  */
 public class InstallWithVersionOverrideMojo
-        extends InstallMojo
-{
+        extends InstallMojo {
 
     public void execute() throws MojoExecutionException {
         try {
@@ -48,23 +51,67 @@ public class InstallWithVersionOverrideMojo
                 if (project != null) {
                     getLog().info("version.override: rewrite the version in the original pom.xom in target/pom.xml");
 
-                    String inputData = IOUtil.toString(new FileInputStream(project.getFile()));
-                    String data = inputData.replace("<version>0.5.3-SNAPSHOT</version>", "<version>"+ versionOverride +"</version>");
-                    FileUtils.fileWrite("target/pom.xml", "UTF-8", data);
                     //it's very important to set the absolute file name as some classes are using File.equals for comparing
                     //to file instances
-                    project.setFile(new File(project.getBasedir(), "target/pom.xml"));
+                    File targetDir = new File(project.getBasedir(), "target");
+                    if (!targetDir.exists()) {
+                        targetDir.mkdir();
+                    }
+                    File newPomFile = new File(targetDir, "pom.xml");
+                    newPomFile.createNewFile();
+                    replaceVersion(project.getFile(), newPomFile, versionOverride);
+                    project.setFile(newPomFile);
                 } else {
                     getLog().warn("could not access the project in InstallMojo: install with version.override did not work!");
                 }
             }
 
-        } catch (IOException e1) {
-            getLog().warn(e1);
+        } catch (IOException e) {
+            getLog().warn(e);
+        } catch (JDOMException e) {
+            getLog().warn(e);
         }
 
         super.execute();
     }
+
+    private void replaceVersion(File originalPomFile, File newPomFile, String newVersion) throws IOException, JDOMException {
+
+        //we assume that the version of "internal" dependencies are declared with ${project.version}
+        FileWriter writer = new FileWriter(newPomFile);
+        SAXBuilder parser = new SAXBuilder();
+        XMLOutputter xmlOutput = new XMLOutputter();
+        // display nice nice
+        xmlOutput.setFormat(Format.getPrettyFormat());
+
+        //parse the document
+        Document doc = parser.build(originalPomFile);
+        Element versionElem = findVersionElement(doc);
+        versionElem.setText(newVersion);
+        xmlOutput.output(doc, writer);
+        writer.flush();
+        writer.close();
+    }
+
+    private Element findVersionElement(Document doc) {
+        for (Element element : doc.getRootElement().getChildren()) {
+            if (element.getName().equals("version")) {
+                return element;
+            }
+        }
+
+        for (Element element : doc.getRootElement().getChildren()) {
+            if (element.getName().equals("parent")) {
+                for (Element childElem : element.getChildren()) {
+                    if (childElem.getName().equals("version")) {
+                        return childElem;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
 
     private MavenProject getMavenProject() {
         try {
