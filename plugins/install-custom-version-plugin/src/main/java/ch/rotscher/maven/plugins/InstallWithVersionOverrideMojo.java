c8275f66900a7d583724561645a47784a20a1017
rotscher <info@rotscher.ch>
2013-09-24 23:10:20 +0200

    get the reference of mavenProject from superclass instead of writing it, fixed bug related to deploy plugin

--- plugins/install-custom-version-plugin/src/main/java/ch/rotscher/maven/plugins/InstallWithVersionOverrideMojo.java
+++ plugins/install-custom-version-plugin/src/main/java/ch/rotscher/maven/plugins/InstallWithVersionOverrideMojo.java
@@ -18,7 +18,6 @@ package ch.rotscher.maven.plugins;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.install.InstallMojo;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.IOUtil;
@@ -39,33 +38,25 @@ import java.lang.reflect.Field;
 public class InstallWithVersionOverrideMojo
         extends InstallMojo
 {
-    @Component
-    private MavenProject project;
 
     public void execute() throws MojoExecutionException {
         try {
 
             String versionOverride = System.getProperty("version.override");
             if (versionOverride != null) {
-                //MavenProject project = getProject();
-                //project
-                String inputData = IOUtil.toString(new FileInputStream(project.getFile()));
-                String data = inputData.replace("<version>0.5.2-SNAPSHOT</version>", "<version>"+ versionOverride +"</version>");
-                FileUtils.fileWrite("target/pom.xml", "UTF-8", data);
-                project.setFile(new File("target/pom.xml"));
+                MavenProject project = getMavenProject();
+                if (project != null) {
+                    getLog().info("version.override: rewrite the version in the original pom.xom in target/pom.xml");
 
-                try {
-                    Field privateStringField = InstallMojo.class.
-                            getDeclaredField("project");
-                        privateStringField.setAccessible(true);
-
-                        privateStringField.set(this, this.project);
-                } catch (NoSuchFieldException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    String inputData = IOUtil.toString(new FileInputStream(project.getFile()));
+                    String data = inputData.replace("<version>0.5.3-SNAPSHOT</version>", "<version>"+ versionOverride +"</version>");
+                    FileUtils.fileWrite("target/pom.xml", "UTF-8", data);
+                    //it's very important to set the absolute file name as some classes are using File.equals for comparing
+                    //to file instances
+                    project.setFile(new File(project.getBasedir(), "target/pom.xml"));
+                } else {
+                    getLog().warn("could not access the project in InstallMojo: install with version.override did not work!");
                 }
-
             }
 
         } catch (IOException e1) {
@@ -73,5 +64,21 @@ public class InstallWithVersionOverrideMojo
         }
 
         super.execute();
+    }
+
+    private MavenProject getMavenProject() {
+        try {
+            Field privateStringField = InstallMojo.class.
+                    getDeclaredField("project");
+            privateStringField.setAccessible(true);
+
+            return (MavenProject) privateStringField.get(this);
+        } catch (NoSuchFieldException e) {
+            getLog().warn(e);
+        } catch (IllegalAccessException e) {
+            getLog().warn(e);
+        }
+
+        return null;
     }
 }
